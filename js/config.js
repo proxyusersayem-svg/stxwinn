ef5063e22242da5b28f9cc2e1f82cd3f10304f8f
royal-casino <proxyusersayem@gmail.com>
2026-07-15 10:25:44 -0700

    Add configuration constants for application

---
+++ js/config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+    KEY_BALANCE: 'stx_win_user_balance',
+    KEY_LOGGED_USER: 'stx_win_logged_in_user',
+    KEY_TRANSACTIONS: 'stx_win_transaction_history',
+    KEY_BETS: 'stx_win_betting_history',
+    KEY_USERS: 'stx_win_users',
+    INIT_BALANCE: 1000.00
+};
